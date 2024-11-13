21a7b7e18ae6bd7e8818d1ca77e1ee99bc833114
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-11-13 18:20:23 -0300

    Adicionar arquivos de projeto.

---
+++ EventosApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CadastroEventosApp.Models;
+using Microsoft.Maui.Controls;
+
+namespace CadastroEventosApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCadastrarEventoClicked(object sender, EventArgs e)
+        {
+            var evento = new Evento
+            {
+                Nome = nomeEventoEntry.Text,
+                DataInicio = dataInicioPicker.Date,
+                DataTermino = dataTerminoPicker.Date,
+                NumeroParticipantes = int.Parse(numeroParticipantesEntry.Text),
+                Local = localEventoEntry.Text,
+                CustoPorParticipante = double.Parse(custoParticipanteEntry.Text)
+            };
+
+            await Navigation.PushAsync(new ResumoEventoPage(evento));
+        }
+    }
+}
---
+++ EventosApp/Views/ResumoPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CadastroEventosApp.Models;
+using Microsoft.Maui.Controls;
+
+namespace CadastroEventosApp
+{
+    public partial class ResumoEventoPage : ContentPage
+    {
+        public ResumoEventoPage(Evento evento)
+        {
+            InitializeComponent();
+
+            nomeEventoLabel.Text = evento.Nome;
+            dataInicioLabel.Text = evento.DataInicio.ToString("dd/MM/yyyy");
+            dataTerminoLabel.Text = evento.DataTermino.ToString("dd/MM/yyyy");
+            numeroParticipantesLabel.Text = evento.NumeroParticipantes.ToString();
+            localLabel.Text = evento.Local;
+            custoParticipanteLabel.Text = evento.CustoPorParticipante.ToString("C");
+            duracaoLabel.Text = evento.DuracaoEmDias.ToString();
+            custoTotalLabel.Text = evento.CustoTotal.ToString("C");
+        }
+    }
+}
